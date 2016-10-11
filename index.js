57b94d06962b556a55c4d0f2c718126289ed7167
Valentin “ThisIsMac” M <thisismac47@gmail.com>
2016-10-11 22:24:47 +0200

    forgot to get the whole body before processing

--- index.js
+++ index.js
@@ -25,40 +25,53 @@ var Worker = function (opts) {
   if (!(this instanceof Worker)) {
     return new Worker(opts);
   }
-  
+
   this.opts = opts;
   this.port = this.opts.port || 8888;
   this.apps = opts.apps;
+
+  if (typeof(this.apps) !== 'object')
+    this.apps = JSON.parse(this.apps);
 
   this.server = http.createServer(this._handleHttp.bind(this));
   return this;
 }
 
 Worker.prototype._handleHttp = function (req, res) {
+  var self = this;
+
   // send instant answer since its useless to respond to the webhook
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('OK');
   res.end();
 
   // do something only with post request
-  if (req.method === 'POST')
-    this.handleRequest(req);
+  if (req.method !== 'POST') return;
+
+  // get the whole body before processing
+  req.body = '';
+  req.on('data', function (data) {
+    req.body += data;
+  }).on('end', function () {
+    self.processRequest(req);
+  });
 }
 
-Worker.prototype.handleRequest = function (req) {
+Worker.prototype.processRequest = function (req) {
   // big security protection here, the legend says that yahoo use these kind.
-  if (!req.headers['X-Github-Event']) return ;
-  if (!req.headers['X-Hub-Signature']) return ;
+  if (!req.headers['x-github-event']) return;
+  if (!req.headers['x-hub-signature']) return;
 
   var target_app = req.url.split('/').pop();
-  if (target_app.length === 0) return ;
+  if (target_app.length === 0) return;
 
   // compute hash of body with secret, github should send this to verify authenticity
   var temp = crypto.createHmac('sha1', this.apps[target_app]);
-  temp.update(body, 'utf-8');
+  temp.update(req.body, 'utf-8');
   var hash = temp.digest('hex');
 
-  if (hash !== req.headers['X-Hub-Signature']) return ;
+  if (hash !== req.headers['x-hub-signature']) 
+    return console.log("Received invalid request for app %s", target_app);
 
   console.log("Received valid request to pull and reload %s", target_app);
   pm2.pullAndGracefulReload(target_app, function (err, data) {
